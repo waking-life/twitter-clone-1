919499836be5a5848a78c7886b7c394e1a31b997
Marla Brizel <marlabrizel@gmail.com>
2015-08-16 09:47:56 -0600

    finish static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe StaticPagesController, type: :controller do
   it "should get home" do
     get :home
     expect(response).to have_http_status(:success)
+    # expect(tag_list).to have_tag("title", "Home | Ruby on Rails Tutorial Sample App")
   end
 
   it "should get help" do
