0b91c6fd4048036b5ccf88dddfca21933286a006
Marla Brizel <marlabrizel@gmail.com>
2015-08-17 10:03:25 -0600

    add contact page

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe StaticPagesController, type: :controller do
+RSpec.describe StaticPagesController, type: :controller do  
   it "should get home" do
     get :home
     expect(response).to have_http_status(:success)
-    # expect(tag_list).to have_tag("title", "Home | Ruby on Rails Tutorial Sample App")
   end
 
   it "should get help" do
@@ -14,6 +13,11 @@ RSpec.describe StaticPagesController, type: :controller do
 
   it "should get about" do
     get :about
+    expect(response).to have_http_status(:success)
+  end
+
+  it "should get contact" do
+    get :contact
     expect(response).to have_http_status(:success)
   end
 end
