7206b0f23f5dd45aa6a113726866411ed5c13fbd
Marla Brizel <marlabrizel@gmail.com>
2015-08-17 16:20:10 -0600

    additional tests

--- spec/features/site_layout_spec.rb
+++ spec/features/site_layout_spec.rb
@@ -5,4 +5,9 @@ RSpec.feature "SiteLayouts", type: :feature do
     visit root_path
     expect(page).to have_content("Help")
   end
+
+  it "can visit the signup page" do
+    visit signup_path
+    expect(page.title).to have_content "Sign up"
+  end
 end
