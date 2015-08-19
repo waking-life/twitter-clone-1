879c155f2809d46214a6e5244c5dfc2d7b92e99d
Marla Brizel <marlabrizel@gmail.com>
2015-08-19 09:17:26 -0600

    add sessions for user login

---
+++ spec/features/user_logins_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature do
+  context "with invalid params" do
+    it "rejects blank params" do
+      visit login_path
+      fill_in "Email", with: ""
+      fill_in "Password", with: ""
+      click_button "Log in"
+
+      expect(current_path).to eq(login_path)
+    end
+  end
+
+  context "with valid params" do
+    let!(:user) { User.create(name: "Bob", email: "bob@email.com", password: "password", password_confirmation: "password") }
+
+    it "successfully logs in" do
+      visit login_path
+      fill_in "Email", with: "bob@email.com"
+      fill_in "Password", with: "password"
+      click_button "Log in"
+
+      expect(current_path).to eq(user_path(user))
+    end
+  end
+end
